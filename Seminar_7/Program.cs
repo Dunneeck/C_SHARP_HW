768db60463299ccf21af9d47059f4bacd397a623
Dunneeck <dunek1997@gmail.com>
2024-01-02 18:08:18 +0300

    hw 8 is done

---
+++ Seminar_7/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+namespace Seminar_7;
+class Program
+{
+    public static TestClass MakeTestclass()
+    {
+        Type testclass = typeof(TestClass);
+        return Activator.CreateInstance(testclass) as TestClass;
+    }
+    public static TestClass MakeTestclass1()
+    {
+        Type testclass = typeof(TestClass);
+        return Activator.CreateInstance(testclass) as TestClass ?? throw new Exception();
+    }
+
+    public static TestClass MakeTestclass(int i)
+    {
+        Type testclass = typeof(TestClass);
+        return Activator.CreateInstance(testclass, new object[] { i }) as TestClass;
+    }
+    public static TestClass MakeTestclass1(int i)
+    {
+        ConstructorInfo ctor = typeof(TestClass).GetConstructors(BindingFlags.Instance | BindingFlags.Instance).First();
+        return (TestClass)ctor.Invoke([i]);
+    }
+
+    public static TestClass MakeTestclass(int i, string s, decimal d, char[] c)
+    {
+        Type testclass = typeof(TestClass);
+        return Activator.CreateInstance(testclass, new object[] { i, s, d, c }) as TestClass;
+    }
+    public static TestClass MakeTestclass1(int i, string s, decimal d, char[] c)
+    {
+        Type tTest = typeof(TestClass);
+        return Activator.CreateInstance(tTest, i,s,d, c) as TestClass ?? throw new Exception();
+    }
+    static object StringToObject(string s)
+    {
+        string[] arr = s.Split("|");
+        string[] arr1 = arr[0].Split("|");
+        object some = Activator.CreateInstance(null, arr[0].Split(",")[0]);
+        if(arr.Length > 1 && some != null)
+        {
+            var type = some.GetType();
+            for (int i = 1; i < arr.Length; i++)
+            {
+                string[] nameAndValue = arr[i].Split(":");
+                var p = type.GetProperty(nameAndValue[0]);
+                if(p != null) { continue; }
+                if(p.PropertyType == typeof(int))
+                {
+                    p.SetValue(some, int.Parse(nameAndValue[1]));
+                }
+                else if (p.PropertyType == typeof(string))
+                {
+                    p.SetValue(some, nameAndValue[1]);
+                }
+                else if(p.PropertyType == typeof(decimal))
+                {
+                    p.SetValue(some, decimal.Parse(nameAndValue[1]));
+                }
+                else if(p.PropertyType == typeof(char[]))
+                {
+                    p.SetValue(some, nameAndValue[1].ToCharArray());
+                }
+            }
+        }
+        return some;
+    }
+    static Object StringToObject1(string s)
+    {
+        Type type = Type.GetType(s) ?? throw new NullReferenceException();
+        return Activator.CreateInstance(type);
+    }
+    static string ObjectToString(object o) { 
+        Type type = typeof(object);
+        StringBuilder sb = new StringBuilder();
+        sb.Append(type.AssemblyQualifiedName + ":");
+        sb.Append(type.Name + '|');
+        var prop = type.GetProperties();
+        foreach (var item in prop)
+        {
+            var tmp = item.GetValue(o);
+            sb.Append(item.Name + ":");
+            if(item.PropertyType == typeof(char[]))
+            {
+                sb.Append(new string(tmp as char[]) + '|');
+            }
+            else
+            {
+                sb.Append(tmp);
+                sb.Append('|');
+            }
+        }
+        return sb.ToString();
+    }
+    static string ObjectToString1(object o)
+    {
+        Type type = o.GetType();
+        StringBuilder sb = new();
+        sb.Append(type.FullName).Append(", ");
+        sb.Append(type.AssemblyQualifiedName).Append(", ");
+        foreach (var field in type.GetProperties())
+        {
+            sb.Append(field.Name).Append(": ");
+            if(field.GetValue(o) is char[] arr)
+                sb.Append(string.Join("", arr));
+            else 
+                sb.Append(field.GetValue(o));
+            sb.Append(", ");
+        }
+        return sb.ToString(); 
+    }
+
+    static void Main()
+    {
+        var r1 = MakeTestclass();
+        //var r2 = MakeTestclass(1);
+        var r3 = MakeTestclass(1, "text", 2, new char[3] { 'a', 'b', 'c' });
+        Console.WriteLine(ObjectToString(r3));
+
+
+        var c1 = MakeTestclass1();
+        //var c2 = MakeTestclass1(1);
+        var c3 = MakeTestclass1(2, "text", 3, ['c', 's']);
+        Console.WriteLine(ObjectToString(c3));
+        ObjectToString1(typeof(TestClass).FullName!);
+    }
+}
---
+++ Seminar_8/Program.cs
@@ -0,0 +1,118 @@
+﻿
+
+
+namespace Seminar_8;
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+        //string str = ReaderFrom(args[0]);
+        //WriteTo(str, args[1]);
+
+        //List<string> list = SearchIn(path: args[0], name: args[1]);
+
+        //var text = ReadFrom(args[0]);
+        //var filtr = Filtr(args[1], text);
+        //Console.WriteLine(String.Join("\n", filtr));
+
+        var files = FindFiles(args[0], args[1]);
+        foreach (var file in files)
+        {
+            ShowResult(file, args[2]);
+        }
+
+
+    }
+    public static List<string> FindFiles(string directoryPath, string extension)
+    {
+        List<string> files = new List<string>();
+        DirectoryInfo directory = new DirectoryInfo(directoryPath);
+
+        if (!directory.Exists)
+        {
+            Console.WriteLine("Directory does not exist.");
+            return files;
+        }
+
+        foreach (FileInfo file in directory.GetFiles("*." + extension))
+        {
+            files.Add(file.FullName);
+            //Console.WriteLine(file.FullName);
+        }
+
+        foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+        {
+            files.AddRange(FindFiles(subDirectory.FullName, extension));
+        }
+
+        return files;
+    }
+    static void ShowResult(string path,  string wordToFind)
+    {
+        using StreamReader file = new StreamReader(path);
+        string line;
+        while ((line = file.ReadLine()!) != null)
+        {
+            if (line.ToLower().Contains(wordToFind.ToLower()))
+            {
+                Console.WriteLine(line.ToLower());
+            }
+        }
+    }
+    static List<string> ReadFrom(string path)
+    {
+        List<string> list = new List<string>();
+        using (StreamReader sr = new StreamReader(path))
+        {
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                list.Add(line); 
+            }
+        }
+        return list;
+    }
+    static List<string> Filtr(string word, List<string> text)
+    {
+        return text.Where(a => a.ToLower().Contains(word.ToLower())).Select(x => x.ToLower().Replace(word.ToLower(), word.ToUpper())).ToList();
+    }
+
+    private static List<string> SearchIn(string path, string name)
+    {
+        var list = new List<string>();
+        DirectoryInfo directory = new DirectoryInfo(path);
+        var directories = directory.GetDirectories();
+        var files = directory.GetFiles();
+        foreach (var item in files)
+        {
+            if (item.Name.Contains(name))
+                list.Add(item.Name);
+            if (item.Extension.Contains(name))
+                list.Add(item.Name);
+        }
+        foreach (var item in directories)
+            list.AddRange(SearchIn(item.FullName, name));
+        return list;
+    }
+
+    private static void WriteTo(string str, string path)
+    {
+        using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+        {
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.WriteLine(str);
+            }
+        }
+    }
+
+    private static string ReaderFrom(string path)
+    {
+        using StreamReader st = new StreamReader(path);
+        return st.ReadToEnd();
+    }
+}
